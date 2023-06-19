40f595595640d1832f8ebc55495be425acbbb822
amaligno <antoine.malignon@yahoo.com>
2023-06-19 18:06:25 +0800

    bonus and mandatory now working, makefile works as well:

--- libft/ft_strcmp.c
+++ libft/ft_strcmp.c
@@ -6,7 +6,7 @@
 /*   By: amaligno <amaligno@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/13 16:56:33 by amaligno          #+#    #+#             */
-/*   Updated: 2023/06/18 20:16:54 by amaligno         ###   ########.fr       */
+/*   Updated: 2023/06/19 17:55:55 by amaligno         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,25 +14,17 @@
 
 int	ft_strcmp(char *s1, char *s2)
 {
-	ft_printf("out s2 str : %s", s2);
-	ft_printf("out s1 : [%c]\n s2 : [%c]\n", *s1, s2[0]);
-	while (*s1 && *s2)
+	while ((*s1 == *s2) && *s1 && *s2)
 	{
-		if (*s1 != *s2)
-		{
-			ft_printf("in s1 : [%c]\n s2 : [%c]\n", *s1, *s2);
-			// ft_printf("in cmp, s1 : [%s]s2 : [%s]\n", &s1[0], &s2[0]);
-			return (1);
-		}
-		if (*(++s1) == '\0' && *(++s2) == '\0')
-			return (0);
-	}
-	return (0);
+		s1++;
+		s2++;
+	}	
+	return (*s1 - *s2);
 }
 
 // int	main(int argc, char  **str)
 // {	
-// 	if (!strcmp("h", "h"))
+// 	if (!ft_strcmp("hello\n", "hello"))
 // 		printf("same\n");
 // 	else
 // 		printf("diff\n");
--- src/do_instructions.c
+++ src/do_instructions.c
@@ -6,7 +6,7 @@
 /*   By: amaligno <amaligno@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/18 16:56:02 by amaligno          #+#    #+#             */
-/*   Updated: 2023/06/18 20:17:44 by amaligno         ###   ########.fr       */
+/*   Updated: 2023/06/19 18:02:40 by amaligno         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,32 +14,28 @@
 
 static int	instructions(t_int **stack_a, t_int **stack_b, char *str)
 {
-	ft_printf("str : %s]", str);
 	if (!ft_strcmp(str, "pa\n"))
 		push(stack_b, stack_a, 'a');
 	else if (!ft_strcmp(str, "pb\n"))
 		push(stack_a, stack_b, 'a');
-	else if (!ft_strcmp(str, str))
-	{
-		ft_printf("hi\n");	
+	else if (!ft_strcmp(str, "sa\n"))
 		swap(stack_a, 'a');
-	}
-	// else if (!ft_strcmp(str, "sb\n"))
-	// 	swap(stack_b, 'b');
-	// else if (!ft_strcmp(str, "ra\n"))
-	// 	rotate(stack_a, 'a');
-	// else if (!ft_strcmp(str, "rb\n"))
-	// 	rotate(stack_b, 'b');
-	// else if (!ft_strcmp(str, "rr\n"))
-	// 	dual_action(stack_a, stack_b, rotate, 'r');
-	// else if (!ft_strcmp(str, "ss\n"))
-	// 	dual_action(stack_a, stack_b, swap, 's');
-	// else if (!ft_strcmp(str, "rra\n"))
-	// 	rev_rotate(stack_a, 'a');
-	// else if (!ft_strcmp(str, "rrb\n"))
-	// 	rev_rotate(stack_b, 'b');
-	// else if (!ft_strcmp(str, "rrr\n"))
-	// 	dual_action(stack_a, stack_b, rotate, 'r');
+	else if (!ft_strcmp(str, "sb\n"))
+		swap(stack_b, 'b');
+	else if (!ft_strcmp(str, "ra\n"))
+		rotate(stack_a, 'a');
+	else if (!ft_strcmp(str, "rb\n"))
+		rotate(stack_b, 'b');
+	else if (!ft_strcmp(str, "rr\n"))
+		dual_action(stack_a, stack_b, rotate, 'r');
+	else if (!ft_strcmp(str, "ss\n"))
+		dual_action(stack_a, stack_b, swap, 's');
+	else if (!ft_strcmp(str, "rra\n"))
+		rev_rotate(stack_a, 'a');
+	else if (!ft_strcmp(str, "rrb\n"))
+		rev_rotate(stack_b, 'b');
+	else if (!ft_strcmp(str, "rrr\n"))
+		dual_action(stack_a, stack_b, rotate, 'r');
 	else
 		return (0);
 	return (1);
@@ -49,22 +45,18 @@ void	do_instructions(t_int **stack_a, t_int **stack_b)
 {
 	char	*str;
 	str = get_next_line(0);
-	ft_printf("str : %s", str);	
 	while (str)
 	{
 		if (!ft_strcmp(str, "\n"))
 		{
-			ft_printf("only nl\n");	
 			free(str);
 			break ;
 		}
 		if (!instructions(stack_a, stack_b, str))
 		{
-			ft_printf("instruct \n");		
 			free(str);
 			break ;
 		}
-		ft_printf("loop str : %s", str);
 		free(str);
 		str = get_next_line(0);
 	}
